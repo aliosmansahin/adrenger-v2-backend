06cefa968ddca7f163c5e13166772c783bbc88e9
aliosmansahin <aliosmsahin01@gmail.com>
2026-02-28 17:35:59 +0300

    validation pipes
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class LoginDto {
+    @IsNotEmpty()
+    @IsEmail()
     email!: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(128)
+    @IsNotEmpty()
     password!: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class RegisterDto {
+    @IsNotEmpty()
+    @IsEmail()
     email!: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(128)
+    @IsNotEmpty()
     password!: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(50)
+    @IsNotEmpty()
     nickname!: string;
 }
